0e47cee59c15d1f5f5738c0f30a7f47cc6942805
ClaydeThabiso <thabisoclayde@gmail.com>
2026-05-08 01:41:49 +0200

    modify the register(remove role select) and update the home page

--- Frontend/src/Authentication/Register.jsx
+++ Frontend/src/Authentication/Register.jsx
@@ -22,7 +22,6 @@ function Register({setUser})
       password:"",
       firstname:"",
       lastname:"",
-      role:""
     })
     const navigate=useNavigate();
     return(
@@ -92,22 +91,7 @@ function Register({setUser})
                 />
               </div>
             </div>
-
-            <div>
-              <label htmlFor="role" className="block text-sm/6 font-medium text-gray-100">
-                Role
-              </label>
-              <div className="mt-2">
-                <select
-                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-gray outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
-                onChange={(e)=> setForm({...form,role:e.target.value})}>
-                  <option value="">Select Role</option>
-                  <option value='M'>Manager</option>
-                  <option value='C'>Customer</option>
-                </select>
-              </div>
-            </div>
-
+            
             <div>
               <div className="flex items-center justify-between">
                 <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
